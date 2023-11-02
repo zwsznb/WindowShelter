5f326e48eabc8796fc86bff746ff6fae88c8f1c0
钟文森 <562009449@qq.com>
2023-11-02 13:54:37 +0800

    feat: init project

---
+++ Whiteboard/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Whiteboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(738, 36);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "白板";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        
+
+        #endregion
+    }
+}
+
---
+++ Whiteboard/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Whiteboard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Point mouseOff;//鼠标移动位置变量
+        bool leftFlag;//标签是否为左键
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            var changePosThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    var rect = GetWeChatWindowRect(this);
+                    Location = new Point(rect.Left - 8, rect.Top);
+                    this.Width = rect.Right;
+                    Thread.Sleep(200);
+                }
+            });
+            //设置为后台线程
+            changePosThread.IsBackground = true;
+            changePosThread.Start();
+        }
+
+        private static User32.Rect GetWeChatWindowRect(Form form)
+        {
+            //获取所有的进程列表
+            Process[] ps = Process.GetProcesses();
+            foreach (Process p in ps)
+            {
+                string info = "";
+                try
+                {
+                    if (p.ProcessName.Equals("WeChat"))
+                    {
+                        //获取窗口句柄
+                        var handle = p.MainWindowHandle;
+
+                        User32.Rect windowRect = new User32.Rect();
+                        var isSuccess = User32.GetWindowRect(handle, ref windowRect);
+                        if (isSuccess)
+                        {
+                            //左上角
+                            Console.WriteLine(windowRect.Top);
+                            Console.WriteLine(windowRect.Left);
+                            //右下角
+                            Console.WriteLine(windowRect.Right);
+                            Console.WriteLine(windowRect.Bottom);
+                            return windowRect;
+                        }
+                        else
+                        {
+                            Console.WriteLine("获取窗口信息失败");
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+                Console.WriteLine(info);
+            }
+            return new User32.Rect();
+        }
+
+        public static class User32
+        {
+            [DllImport("User32.dll")]
+            public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct Rect
+            {
+                public int Left;
+                public int Top;
+                public int Right;
+                public int Bottom;
+            }
+        }
+    }
+}
