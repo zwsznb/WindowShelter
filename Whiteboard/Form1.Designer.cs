dcb26035841881f9769e20055c60d88347451b1c
“zwsznb” <562009449@qq.com>
2023-11-04 09:55:09 +0800

    refactor: 更改默认图片

--- Whiteboard/Form1.Designer.cs
+++ Whiteboard/Form1.Designer.cs
@@ -37,7 +37,6 @@ namespace Whiteboard
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -46,26 +45,27 @@ namespace Whiteboard
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(736, 44);
+            this.pictureBox1.Size = new System.Drawing.Size(981, 55);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(736, 44);
+            this.ClientSize = new System.Drawing.Size(981, 55);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "白板";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1Load);
             this.Resize += new System.EventHandler(this.Form1Resize);
             this.Disposed += new System.EventHandler(this.Form1Dispose);
-            this.KeyDown += KeyClose;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
